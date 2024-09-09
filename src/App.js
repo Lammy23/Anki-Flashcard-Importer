6533ea066ecf40ef9422df9a0946ee1281abfb62
Olamikun <lammyaluko@gmail.com>
2024-09-08 19:07:35 -0600

    sylized app complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
+// src/App.js
 import { useState } from "react";
-import { test, createModel, createDeck } from "./apiService";
+import { createModel, createDeck } from "./apiService";
 import "./App.css";
 
 function App() {
-  const [model, setModel] = useState();
-  const [deck, setDeck] = useState();
-  const [parentDeck, setParentDeck] = useState();
+  const [model, setModel] = useState("");
+  const [deck, setDeck] = useState("");
+  const [parentDeck, setParentDeck] = useState("");
 
   return (
     <div className="app">
@@ -14,7 +15,7 @@ function App() {
           <p>Model</p>
           <textarea
             name="model"
-            rows="40"
+            rows="10"
             cols="60"
             onChange={(e) => setModel(e.target.value)}
           />
@@ -24,18 +25,19 @@ function App() {
           <p>Deck</p>
           <textarea
             name="decks"
-            rows="40"
+            rows="10"
             cols="60"
             onChange={(e) => setDeck(e.target.value)}
           />
         </div>
       </div>
       <div className="column">
-        <p>parentDeck</p>
+        <p>Parent Deck</p>
         <input
           name="parentDeck"
           type="text"
           placeholder="deck 1::deck 2:: ..."
+          onChange={(e) => setParentDeck(e.target.value)}
         />
       </div>
       <div className="row">
