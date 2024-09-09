5c351387a6f2fb2bc58b0f1fded05bcf50cbcc44
Olamikun <lammyaluko@gmail.com>
2024-09-08 19:01:54 -0600

    bare bones app completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { test, createModel, createDeck } from "./apiService";
+import "./App.css";
 
 function App() {
+  const [model, setModel] = useState();
+  const [deck, setDeck] = useState();
+  const [parentDeck, setParentDeck] = useState();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="app">
+      <div className="row">
+        <div className="column">
+          <p>Model</p>
+          <textarea
+            name="model"
+            rows="40"
+            cols="60"
+            onChange={(e) => setModel(e.target.value)}
+          />
+        </div>
+
+        <div className="column">
+          <p>Deck</p>
+          <textarea
+            name="decks"
+            rows="40"
+            cols="60"
+            onChange={(e) => setDeck(e.target.value)}
+          />
+        </div>
+      </div>
+      <div className="column">
+        <p>parentDeck</p>
+        <input
+          name="parentDeck"
+          type="text"
+          placeholder="deck 1::deck 2:: ..."
+        />
+      </div>
+      <div className="row">
+        <button
+          onClick={() => {
+            const { modelName, inOrderFields } = createModel(model);
+            createDeck(deck, modelName, inOrderFields, parentDeck);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Upload
+        </button>
+      </div>
     </div>
   );
 }
