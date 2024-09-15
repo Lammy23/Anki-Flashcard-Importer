839c08f100bb3c0060aacc060aa4b9467909d81b
Olamikun <lammyaluko@gmail.com>
2024-09-15 00:44:47 -0600

    ss

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Header = () => (
   <div className="header">
-    <h1>Anki Importer</h1>
-    <p>Create and manage your Anki decks with ease</p>
+    {/* <h1>Anki Importer</h1>
+    <p>Create and manage your Anki decks with ease</p> */}
   </div>
 );
 
